140f68e47c458790bbb8461408f9f0500be11b06
Marco Escaleira <marcoescaleira2000@gmail.com>
2020-02-23 13:42:14 +0000

    added cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
+const cors = require("cors");
 
 // Start app
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Start DB
 const mongodbPort = 27017;
